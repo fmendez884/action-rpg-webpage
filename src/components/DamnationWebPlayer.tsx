7e020ae32b83fad1bd3ba4372d4c7cf89d6774da
Francis <fmendez884@gmail.com>
2020-06-21 23:30:20 -0700

    Adds unity webplayer in html

---
+++ src/components/DamnationWebPlayer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom"
+import { connect } from "react-redux";
+
+export default class DamnationWebPlayer extends React.Component {
+    render() {
+        return (
+            
+                <div id="unityContainer" style={{width: 960, height: 600}}></div>
+          
+        );
+    }
+}
